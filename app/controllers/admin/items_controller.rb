4acdb42759f0db8d1d7a8b1ba638a1c2b067c53c
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-06-20 01:10:31 +0000

    販売ステータス

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,28 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
+  
 
   def new
+    @item = Item.new
+  end
+
+  def index
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(:id)
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image,:is_active)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
     has_one_attached :image
     belongs_to :genre
+    
+    enum is_active: { sale:0 ,stop_sale:1 }
 end
