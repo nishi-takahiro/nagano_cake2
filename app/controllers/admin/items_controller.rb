cb908e8beeb7cfff1606d8247cfddc3799fb50ac
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-06-21 22:39:51 +0000

    商品詳細

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,9 +9,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
-   
    @item = Item.find(params[:id])
-   binding.pry
   end
 
   def edit
@@ -35,6 +33,6 @@ class Admin::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image, :is_active, :genre_id)
+    params.require(:item).permit(:name, :introduction, :price, :image, :is_active, :genre_id, :is_active)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
     has_one_attached :image
     belongs_to :genre
     
-    enum is_active: { sale:0 ,stop_sale:1 }
+    
 end
