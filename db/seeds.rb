a92b3d724f6a0b4c2d4b86428f8046e8500b324d
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-06-23 23:31:14 +0000

    カスタマー編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,50 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#管理者
 Admin.create!(
    email: 'admin@admin',
    password: '7777777',
    password_confirmation: '7777777',
 )
 
-Customer.create!(
-   last_name: '太郎',
-   first_name: '奈良',
-   last_name_kana: 'タロウ',
-   first_name_kana: 'ナラ',
-   email: '00@gmail.com',
-   encrypted_password: 'aaaaaa',
-   postal_code: '0000001',
-   address: '奈良県',
-   telephone_number: '000000000001',
-   password: 'aaaaaa',
-   )
-   
-Customer.create!(
-   last_name: 'のび太',
-   first_name: 'のび',
-   last_name_kana: 'ノビタ',
-   first_name_kana: 'ノビ',
-   email: '01@gmail.com',
-   encrypted_password: 'aaaaaa',
-   postal_code: '0000002',
-   address: '東京都',
-   telephone_number: '000000000002',
-   password: 'aaaaaa',
-   )
+#ユーザー
+Customer.create!(first_name: '奈良', last_name: '太郎', first_name_kana: 'ナラ', last_name_kana: 'タロウ', email: '00@gmail.com', encrypted_password: 'aaaaaa', postal_code: '0000001', address: '奈良県', telephone_number: '000000000001', password: 'aaaaaa',)
+Customer.create!(first_name: 'のび',last_name: 'のび太', first_name_kana: 'ノビ', last_name_kana: 'ノビタ', email: '01@gmail.com', encrypted_password: 'aaaaaa', postal_code: '0000002', address: '東京都', telephone_number: '000000000002', password: 'aaaaaa',)
 
-Genre.create!(
-   name: 'ケーキ'
-   )
-   
-Genre.create!(
-   name: 'プリン'
-   )
-   
-Genre.create!(
-   name: '焼き菓子'
-   )
-   
-Genre.create!(
-   name: 'キャンディ'
-   )
+#ジャンル
+Genre.create!(name: 'ケーキ')
+Genre.create!(name: 'プリン')
+Genre.create!(name: '焼き菓子')
+Genre.create!(name: 'キャンディ')
