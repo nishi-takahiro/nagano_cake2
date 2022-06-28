91cb2d2c2099eb8ebeaf918a8f20439421d03855
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-06-28 00:34:59 +0000

    カートアイテム個数

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,3 +2,12 @@ class Public::CartItemsController < ApplicationController
   def index
   end
 end
+
+def create
+ @item = Item.find(cart_item.params[:item_id])
+end
+
+private
+def cart_item_params
+  params.require(:cart_item).permit(:item_id, :amount)
+end
