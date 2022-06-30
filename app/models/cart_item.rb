c7a6e9afc2ce13f27fe6b9f992270a425e7bede5
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-06-30 01:48:56 +0000

    カート一覧

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,15 @@
 class CartItem < ApplicationRecord
-     has_one_attached :image
+    has_one_attached :image
     belongs_to :customer
     belongs_to :item
+    
+    #消費税の計算
+    def add_tax_price
+        (self.price*1.10).round
+    end
+    
+    #小計の計算
+    def subtotal
+        item.add_tax_price * amount
+    end
 end
