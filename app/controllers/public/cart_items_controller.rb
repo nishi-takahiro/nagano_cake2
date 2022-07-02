c1e5023ddc3db4d7676a09a559a791a6feecaad2
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-07-02 02:18:37 +0000

    カート機能修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -35,7 +35,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-   
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path(@cart_item)
   end 
 
 private
